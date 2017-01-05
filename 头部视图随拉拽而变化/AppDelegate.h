95cc1f7522cc2297666bb49f3160544fb3c01e46
纵昂 <zongang@zongangdemacbook-pro.local>
2017-01-05 18:41:45 +0800

    Initial Commit

---
+++ 头部视图随拉拽而变化/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  头部视图随拉拽而变化
+//
+//  Created by 纵昂 on 17/1/5.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 头部视图随拉拽而变化/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  头部视图随拉拽而变化
+//
+//  Created by 纵昂 on 17/1/5.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
